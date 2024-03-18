8ae649b9932ce82eb3939cf506a11b28327ec5ef
Onur Ucar <hello@ucaronur.com>
2024-03-18 02:07:35 -0400

    Initial view implementation

--- NotesApp Watch App/View/ContentView.swift
+++ NotesApp Watch App/View/ContentView.swift
@@ -8,15 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Property
+    @State private var notes: [Note] = [Note]()
+    @State private var text: String = ""
+
+    // Function
+
+    // Body
+
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack(alignment: .center, spacing: 6){
+                    TextField("Add a new note", text: $text)
+                    
+                    Button {
+                        //Action will go here
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 42, weight: .semibold))
+                    }
+                    // this is required to make the button smaller and not occupy
+                    // the same amount of space as the text field
+                    .fixedSize()
+                    .buttonStyle(PlainButtonStyle())
+                    .foregroundColor(.accentColor)
+                    //.buttonStyle(BorderedButtonStyle(tint: .accentColor))
+                }
+                Spacer()
+            }
+            .navigationTitle("Notes")
         }
-        .padding()
     }
+
+    // Preview
 }
 
 #Preview {
