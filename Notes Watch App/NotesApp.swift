0f09bd16c1b4d5478026e91e22e4ffddaae8858b
Onur Ucar <hello@ucaronur.com>
2024-03-18 01:36:10 -0400

    initial commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Onur Ucar on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
