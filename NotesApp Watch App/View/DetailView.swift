801fa853ee8f3a3b5933f711acbd7a01a5f77cdb
Onur Ucar <hello@ucaronur.com>
2024-03-20 01:19:59 -0400

    Implemented detail and settings views

---
+++ NotesApp Watch App/View/DetailView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailView.swift
+//  NotesApp Watch App
+//
+//  Created by Onur Ucar on 3/20/24.
+//
+
+import Foundation
---
+++ NotesApp Watch App/View/SettingsView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsView.swift
+//  NotesApp Watch App
+//
+//  Created by Onur Ucar on 3/20/24.
+//
+
+import Foundation
