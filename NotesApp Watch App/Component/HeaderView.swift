466f9977a0c697934d305dda651fab7b05d19f63
Onur Ucar <hello@ucaronur.com>
2024-03-20 01:45:30 -0400

    Done implementing the base features of the app

---
+++ NotesApp Watch App/Component/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  NotesApp Watch App
+//
+//  Created by Onur Ucar on 3/20/24.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
